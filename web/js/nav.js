4dc4a72da442d928c02d0283e340720babb5459d
Bulava Eduard <bulavaeduard@gmail.com>
2016-12-12 20:20:39 +0200

    navigation fixed

--- web/js/nav.js
+++
@@ -1,8 +0,0 @@
-$('button').on("click", function() {
-    $(this).toggleClass('is-closed');
-    if(!$(this).hasClass('is-closed')) {
-        $('body').addClass('is-open');
-    } else {
-        $('body').removeClass('is-open');
-    }
-});
